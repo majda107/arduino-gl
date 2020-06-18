f038c707ce39ff1829e11748744c5c789ecbd9ee
majda107 <majda.trpkos@gmail.com>
2020-06-18 17:30:19 +0200

    Working ILI9163C example

--- majda_gl/majda_gl.ino
+++ majda_gl/majda_gl.ino
@@ -1,40 +1,10 @@
-//Technical support:goodtft@163.com
-// Breakout/Arduino UNO pin usage:
-// LCD Data Bit :   7   6   5   4   3   2   1   0
-// Uno dig. pin :   7   6   5   4   3   2   9   8
-// Uno port/pin : PD7 PD6 PD5 PD4 PD3 PD2 PB1 PB0
-// Mega dig. pin:  29  28  27  26  25  24  23  22
+#include "ILI9163C_TFT.h"
 
-#include <BitBool.h>
+#define __CS  10
+#define __RS  8
+#define __DC  9
 
-#define BLACK   0x0000
-#define BLUE    0x001F
-#define RED     0xF800
-#define GREEN   0x07E0
-#define CYAN    0x07FF
-#define MAGENTA 0xF81F
-#define YELLOW  0xFFE0
-#define WHITE   0xFFFF
-#define GRAY    0xF0F0
-
-
-
-#define LCD_RD   A0
-#define LCD_WR   A1
-#define LCD_RS   A2
-#define LCD_CS   A3
-#define LCD_REST A4
-#define WR_MASK B00000010
-#define RS_MASK B00000100
-
-const unsigned short WIDTH = 320;
-const unsigned short HEIGHT = 480;
-
-//short min_x = 0;
-//short min_y = 0;
-//short max_x = 0;
-//short max_y = 0;
-
+ILI9163C_TFT tft = ILI9163C_TFT(__CS, __RS, __DC);
 
 
 
@@ -71,200 +41,8 @@ struct mat4f
 
 
 
-void Lcd_Writ_Bus(unsigned char d)
-{
-  PORTD = (PORTD & B00000011) | ((d) & B11111100);
-  PORTB = (PORTB & B11111100) | ((d) & B00000011);
-  PORTC &= ~WR_MASK;
-  PORTC |=  WR_MASK;
-}
-
-void Lcd_Write_Com(unsigned char VH)
-{
-  PORTC &= ~RS_MASK;
-  Lcd_Writ_Bus(VH);
-}
-
-void Lcd_Write_Data(unsigned char VH)
-{
-  PORTC |= RS_MASK;
-  Lcd_Writ_Bus(VH);
-}
-
-void Lcd_Write_Com_Data(unsigned char com, unsigned char dat)
-{
-  Lcd_Write_Com(com);
-  Lcd_Write_Data(dat);
-}
-
-void Address_set(unsigned int x1, unsigned int y1, unsigned int x2, unsigned int y2)
-{
-  Lcd_Write_Com(0x2a);
-  Lcd_Write_Data(x1 >> 8);
-  Lcd_Write_Data(x1);
-  Lcd_Write_Data(x2 >> 8);
-  Lcd_Write_Data(x2);
-  Lcd_Write_Com(0x2b);
-  Lcd_Write_Data(y1 >> 8);
-  Lcd_Write_Data(y1);
-  Lcd_Write_Data(y2 >> 8);
-  Lcd_Write_Data(y2);
-  Lcd_Write_Com(0x2c);
-}
 
 
-void Lcd_Init(void)
-{
-  digitalWrite(LCD_REST, HIGH);
-  delay(5);
-  digitalWrite(LCD_REST, LOW);
-  delay(15);
-  digitalWrite(LCD_REST, HIGH);
-  delay(15);
-
-  digitalWrite(LCD_CS, HIGH);
-  digitalWrite(LCD_WR, HIGH);
-  digitalWrite(LCD_CS, LOW); //CS
-
-  Lcd_Write_Com(0x11);
-  delay(20);
-  Lcd_Write_Com(0xD0);
-  Lcd_Write_Data(0x07);
-  Lcd_Write_Data(0x42);
-  Lcd_Write_Data(0x18);
-
-  Lcd_Write_Com(0xD1);
-  Lcd_Write_Data(0x00);
-  Lcd_Write_Data(0x07);//07
-  Lcd_Write_Data(0x10);
-
-  Lcd_Write_Com(0xD2);
-  Lcd_Write_Data(0x01);
-  Lcd_Write_Data(0x02);
-
-  Lcd_Write_Com(0xC0);
-  Lcd_Write_Data(0x10);
-  Lcd_Write_Data(0x3B);
-  Lcd_Write_Data(0x00);
-  Lcd_Write_Data(0x02);
-  Lcd_Write_Data(0x11);
-
-  Lcd_Write_Com(0xC5);
-  Lcd_Write_Data(0x03);
-  Lcd_Write_Com(0x36);
-  Lcd_Write_Data(0x0A);
-
-  Lcd_Write_Com(0x3A);
-  Lcd_Write_Data(0x55);
-
-  Lcd_Write_Com(0x2A);
-  Lcd_Write_Data(0x00);
-  Lcd_Write_Data(0x00);
-  Lcd_Write_Data(0x01);
-  Lcd_Write_Data(0x3F);
-
-  Lcd_Write_Com(0x2B);
-  Lcd_Write_Data(0x00);
-  Lcd_Write_Data(0x00);
-  Lcd_Write_Data(0x01);
-  Lcd_Write_Data(0xE0);
-  delay(120);
-  Lcd_Write_Com(0x29);
-  Lcd_Write_Com(0x002c);
-}
-
-
-
-
-
-void LCD_Pixel(short x, short y, unsigned int c)
-{
-  Address_set(x, y, x + 1, y + 1);
-
-  Lcd_Write_Data(c >> 8);
-  Lcd_Write_Data(c);
-}
-
-
-
-void LCD_Clear(unsigned int j)
-{
-  unsigned int i, m;
-  digitalWrite(LCD_RS, HIGH);
-  digitalWrite(LCD_CS, LOW);
-  Address_set(0, 0, WIDTH, HEIGHT);
-  Lcd_Write_Com(0x02c); //write_memory_start
-
-
-
-  for (i = 0; i < HEIGHT; i++)
-    for (m = 0; m < WIDTH; m++)
-    {
-      Lcd_Write_Data(j >> 8);
-      Lcd_Write_Data(j);
-
-    }
-  digitalWrite(LCD_CS, HIGH);
-  digitalWrite(LCD_RS, LOW);
-}
-
-
-
-
-
-void SetPixel(int x, int y, unsigned int color, bool clean)
-{
-
-  //if(x < min_x) min_x = x;
-  //if(y < min_y) min_y = y;
-
-  //if(x > max_x) max_x = x;
-
-
-
-  //Lcd_Write_Com(0x02c);
-  //digitalWrite(LCD_RS,HIGH);
-  digitalWrite(LCD_CS, LOW);
-
-  LCD_Pixel(x, y, color);
-  digitalWrite(LCD_CS, HIGH);
-}
-
-
-
-
-
-void DrawLine(short x1, short y1, short x2, short y2, unsigned int color, bool clean)
-{
-  short dx = abs(x2 - x1);
-  short sx = x1 < x2 ? 1 : -1;
-
-  short dy = -abs(y2 - y1);
-  short sy = y1 < y2 ? 1 : -1;
-
-  short err = dx + dy;
-  short e2;
-
-  while (true)
-  {
-    SetPixel(x1, y1, color, clean);
-    if (x1 == x2 && y1 == y2)
-      break;
-
-    e2 = 2 * err;
-    if (e2 >= dy)
-    {
-      err += dy;
-      x1 += sx;
-    }
-
-    if (e2 <= dx)
-    {
-      err += dx;
-      y1 += sy;
-    }
-  }
-}
 
 void DrawTriangle(triangle t, unsigned int color, bool clean) {
   DrawTriangle((short)t.p[0].x, (short)t.p[0].y, (short)t.p[1].x, (short)t.p[1].y, (short)t.p[2].x, (short)t.p[2].y, color, clean);
@@ -272,9 +50,9 @@ void DrawTriangle(triangle t, unsigned int color, bool clean) {
 
 void DrawTriangle(short x0, short y0, short x1, short y1, short x2, short y2, unsigned int color, bool clean)
 {
-  DrawLine(x0, y0, x1, y1, color, clean);
-  DrawLine(x1, y1, x2, y2, color, clean);
-  DrawLine(x2, y2, x0, y0, color, clean);
+  tft.draw_line(x0, y0, x1, y1, color);
+  tft.draw_line(x1, y1, x2, y2, color);
+  tft.draw_line(x2, y2, x0, y0, color);
 }
 
 
@@ -348,7 +126,7 @@ void load() {
   float f_near = 0.1f;
   float f_far = 1000.0f;
   float fov = 90.0f * 3.14159f / 180.0f;
-  float aspect = (float)HEIGHT / (float)WIDTH;
+  float aspect = (float)tft.HEIGHT / (float)tft.WIDTH;
 
   mat_proj.m[0][0] = fov * aspect;
   mat_proj.m[1][1] = fov;
@@ -406,9 +184,9 @@ void render_loop(unsigned int color, bool clean) {
       t_proj.p[v] = multiply_mat4_vec3(t.p[v], mat_z);
       t_proj.p[v] = multiply_mat4_vec3(t_proj.p[v], mat_x);
 
-      t_proj.p[v].x += 1.0f;
-      t_proj.p[v].y += 1.0f;
-      t_proj.p[v].z += 3.0f;
+      t_proj.p[v].x += 0.0f;
+      t_proj.p[v].y += 0.0f;
+      t_proj.p[v].z += 3.25f;
 
       t_proj.p[v] = multiply_mat4_vec3(t_proj.p[v], mat_proj);
 
@@ -416,8 +194,8 @@ void render_loop(unsigned int color, bool clean) {
       t_proj.p[v].x += 1.0f;
       t_proj.p[v].y += 1.0f;
 
-      t_proj.p[v].x *= 0.5f * (float)WIDTH / 2;
-      t_proj.p[v].y *= 0.5f * (float)HEIGHT / 2;
+      t_proj.p[v].x *= 0.5f * (float)tft.WIDTH;
+      t_proj.p[v].y *= 0.5f * (float)tft.HEIGHT;
     }
 
     DrawTriangle(t_proj, color, clean);
@@ -430,26 +208,8 @@ void render_loop(unsigned int color, bool clean) {
 
 void setup()
 {
-  //Serial.begin(9600);
-
-  for (int p = 0; p < 10; p++)
-    pinMode(p, OUTPUT);
-
-  pinMode(A0, OUTPUT);
-  pinMode(A1, OUTPUT);
-  pinMode(A2, OUTPUT);
-  pinMode(A3, OUTPUT);
-  pinMode(A4, OUTPUT);
-  digitalWrite(A0, HIGH);
-  digitalWrite(A1, HIGH);
-  digitalWrite(A2, HIGH);
-  digitalWrite(A3, HIGH);
-  digitalWrite(A4, HIGH);
-  Lcd_Init();
-
-  //LCD_Clear(BLACK);
-
-  //load();
+  tft.start();
+  load();
 }
 
 
@@ -460,16 +220,10 @@ void setup()
 
 
 void loop() {
+  tft.fill_screen(BLACK);
 
-  //theta += 0.2f;
-  //render_loop(WHITE, false);
+  theta += 0.2f;
+  render_loop(WHITE, false);
 
-  //theta -= 0.2f;
-  //render_loop(BLACK, true);
-
-  //theta += 0.2f;
-
-
-  
-  ////delay(14);
+  delay(14);
 }
